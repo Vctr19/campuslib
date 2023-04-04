5b6d8a2ef16c47fc01fa8d06dae4d92dca3f225a
Victor <victor.rmsp@gmail.com>
2023-04-04 20:29:46 -0300

    [add] user not found exception handler

--- src/main/java/br/edu/impacta/campuslib/exception/UserNotFoundException.java
+++ src/main/java/br/edu/impacta/campuslib/exception/UserNotFoundException.java
@@ -1,2 +1,7 @@
-package br.edu.impacta.campuslib.exception;public class UserNotFoundException {
+package br.edu.impacta.campuslib.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
