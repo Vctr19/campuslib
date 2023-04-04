afac420d1bc0ff7d85d00f47c3b65dbc388de4b7
Victor Ramos <victor.rmsp@gmail.com>
2023-04-04 00:37:42 -0300

    bootstrapping spring boot

---
+++ src/main/java/br/edu/impacta/campuslib/CampuslibApplication.java
@@ -0,0 +1,13 @@
+package br.edu.impacta.campuslib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampuslibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampuslibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/impacta/campuslib/CampuslibApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.impacta.campuslib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampuslibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
