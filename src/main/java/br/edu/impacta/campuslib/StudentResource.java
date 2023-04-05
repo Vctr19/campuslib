f809283fc7a75b4a05fe1d4449fb897e33be8d58
Victor <victor.rmsp@gmail.com>
2023-04-04 22:55:25 -0300

    [add] student resource class

---
+++ src/main/java/br/edu/impacta/campuslib/StudentResource.java
@@ -0,0 +1,38 @@
+package br.edu.impacta.campuslib;
+
+import br.edu.impacta.campuslib.model.Student;
+import br.edu.impacta.campuslib.service.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/student")
+public class StudentResource {
+    private final StudentService studentService;
+
+    public StudentResource(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Student>> getAllStudents(){
+        List<Student> students = studentService.findAllStudents();
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable("id") UUID id){
+        Student student = studentService.findStudentById(id);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Student> addStudent(@RequestBody Student student){
+        Student newStudent = studentService.addStudent(student);
+        return new ResponseEntity<>(newStudent, HttpStatus.CREATED);
+    }
+}
